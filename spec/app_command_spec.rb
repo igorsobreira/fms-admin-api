b0bcdfc50b3b6ec32fc3feb366781bed4c68c7db
Igor Sobreira <igor@igorsobreira.com>
2011-12-06 23:06:16 -0200

    Creates an rspec matcher keep tests simpler

--- spec/app_command_spec.rb
+++ spec/app_command_spec.rb
@@ -6,32 +6,29 @@ describe 'AppCommand' do
   describe 'list' do
 
     it 'should build the correct url' do
-      stub_request(:get, "http://fms.example.com:1111/admin/getApps?apswd=secret&auser=fms&force=false&verbose=true")
-
       options = ["--host=fms.example.com:1111", 
                  "--user=fms", 
                  "--password=secret"]
-      invoke_command("app", "list", options).should 
+
+      ["app", "list", options].should build_url("http://fms.example.com:1111/admin/getApps?apswd=secret&auser=fms&force=false&verbose=true")
     end
 
     it 'should build the correct url using force, verbose is implicit in this case' do
-      stub_request(:get, "http://fms.example.com:1111/admin/getApps?apswd=secret&auser=fms&force=true&verbose=true")
-      
       options = ["--host=fms.example.com:1111", 
                  "--user=fms", 
                  "--password=secret",
                  "--force"]
-      invoke_command("app", "list", options)
+
+      ["app", "list", options].should build_url("http://fms.example.com:1111/admin/getApps?apswd=secret&auser=fms&force=true&verbose=true")                   
     end
 
     it 'should build the correct url using verbose' do
-      stub_request(:get, "http://fms.example.com:1111/admin/getApps?apswd=secret&auser=fms&force=false&verbose=true")
-
       options = ["--host=fms.example.com:1111",
                  "--user=fms",
                  "--password=secret",
                  "--verbose"]
-      invoke_command("app", "list", options)
+
+      ["app", "list", options].should build_url("http://fms.example.com:1111/admin/getApps?apswd=secret&auser=fms&force=false&verbose=true")
     end
 
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,3 +11,31 @@ def invoke_command(command, subcommand, options)
   cli = FMSAdmin::CLI.new(args, options)
   cli.invoke(commands[command.to_sym], args)
 end
+
+
+#
+# This matcher works by stubing the expected request url,
+# and since WebMock raises and exception if any other url 
+# is asked, I can tell if the test has built the wrong url
+#
+# The sad thing is that there is no way to get the requested
+# url from WebMock API, so I parse the exception string...
+#
+RSpec::Matchers.define :build_url do |expected|
+  match do |actual|
+    matched = true
+    stub_request(:get, expected)
+
+    begin
+      invoke_command(*actual)
+    rescue WebMock::NetConnectNotAllowedError => error
+      got_url = error.message.split(" ")[8]
+      failure_message_for_should do |actual|
+        "expected #{expected.inspect}\nbut got: #{got_url.inspect}"
+      end
+      matched = false
+    end
+
+    matched
+  end
+end
