d108e8f8fe3e43ea6de7b7aacc73dc65a9589834
Igor Sobreira <igor@igorsobreira.com>
2011-12-07 22:36:45 -0200

    Replace rspec tests by minitest, simpler and enough

---
+++ tests/app_command_tests.rb
@@ -0,0 +1,66 @@
+require_relative './tests_helper'
+
+class AppCommandTests < BaseTestCase
+
+  def setup
+    stub_request(:get, /.*/)
+  end
+
+  def teardown
+    WebMock.reset!
+  end
+
+  def test_list_command_should_build_correct_url
+    options = ["--host=fms.example.com:1111", 
+               "--user=fms", 
+               "--password=secret"]
+    invoke_command('app', 'list', options)
+    assert_requested(:get, "http://fms.example.com:1111/admin/getApps?apswd=secret&auser=fms&force=false&verbose=true")    
+  end
+
+  def test_list_command_should_build_correct_url_using_force_and_verbose_is_implicit
+    options = ["--host=fms.example.com:1111", 
+               "--user=fms", 
+               "--password=secret",
+               "--force"]    
+    invoke_command('app', 'list', options)
+    assert_requested(:get, "http://fms.example.com:1111/admin/getApps?apswd=secret&auser=fms&force=true&verbose=true")    
+  end
+
+  def test_list_command_should_build_correct_url_using_verbose
+    options = ["--host=fms.example.com:1111",
+               "--user=fms",
+               "--password=secret",
+               "--verbose"]
+    invoke_command('app', 'list', options)
+    assert_requested(:get, "http://fms.example.com:1111/admin/getApps?apswd=secret&auser=fms&force=false&verbose=true")
+  end
+
+  def test_reload_command_should_build_correct_url
+    options = ["--host=fms.example.com:1111",
+               "--user=fms",
+               "--password=secret",
+               "--app=live"]
+    invoke_command('app', 'reload', options)
+    assert_requested(:get, "http://fms.example.com:1111/admin/reloadApp?apswd=secret&auser=fms&appInst=live")
+  end
+
+  def test_stats_command_should_build_the_correct_url
+    options = ["--host=fms.example.com:1111", 
+               "--user=fms", 
+               "--password=secret",
+               "--app=live"]
+    invoke_command('app', 'stats', options)
+    assert_requested(:get, "http://fms.example.com:1111/admin/getAppStats?apswd=secret&auser=fms&app=live")
+  end
+
+  def test_unload_command_should_build_correct_url
+    options =   ["--host=fms.example.com:1111", 
+                 "--user=fms", 
+                 "--password=secret",
+                 "--app=live"]
+    invoke_command('app', 'unload', options)
+    assert_requested(:get, "http://fms.example.com:1111/admin/unloadApp?apswd=secret&auser=fms&appInst=live")
+  end
+
+end
---
+++ tests/tests_helper.rb
@@ -0,0 +1,20 @@
+$:.unshift File.expand_path('../lib', File.dirname(__FILE__))
+
+require 'minitest/autorun'
+require 'webmock'
+
+require 'fmsadmin/cli'
+
+class BaseTestCase < MiniTest::Unit::TestCase
+  include WebMock::API
+
+  def invoke_command(command, subcommand, options)
+    args = [subcommand]
+    commands = {
+      :app => FMSAdmin::Command::App,
+    }
+    cli = FMSAdmin::CLI.new(args, options)
+    cli.invoke(commands[command.to_sym], args)
+  end
+
+end
