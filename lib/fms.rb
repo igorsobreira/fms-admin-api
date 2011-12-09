79879ea5468f6e6f6d122a37604bfb5791bc9398
Igor Sobreira <igor@igorsobreira.com>
2011-12-08 23:22:44 -0200

    Change client implementation. Methods are build on demand using `method_missing`.
    The command line interface was removed for now, it's be generated also, similar to FMS::Client

--- lib/fms.rb
+++ lib/fms.rb
@@ -1,6 +1,4 @@
 require 'fms/client'
-require 'fms/api_method'
-require 'fms/api'
 
 module FMS
 end
--- tests/client_tests.rb
+++ tests/client_tests.rb
@@ -6,25 +6,38 @@ class ApiMethodTests < BaseTestCase
     stub_request(:get, /.*/)
   end
 
-  def test_should_accept_host_user_password_with_default_port_and_build_params
-    c = FMS::Client.new("fms.example.com", "fms", "secret")
+  def teardown
+    WebMock.reset!
+  end
 
+  def test_should_require_host_parameter_and_provide_defaults_to_others
+    c = FMS::Client.new(:host => "fms.example.com")
     assert_equal("fms.example.com", c.host)
     assert_equal(1111, c.port)
-    assert_equal({:auser => "fms", :apswd => "secret"}, c.params)
+    assert_equal({:auser => "fms", :apswd => "fms"}, c.base_params)
   end
 
-  def test_should_customize_port
-    c = FMS::Client.new("fms.example.com", "fms", "secret", 2222)
-
-    assert_equal(2222, c.port)    
+  def test_should_raise_error_if_host_not_provided
+    assert_raises(ArgumentError) { FMS::Client.new }
   end
 
-  def test_get_apps_method_should_build_correct_url
-    c = FMS::Client.new("fms.example.com", "fms", "secret")
+  def test_should_build_url_based_on_method_name
+    c = FMS::Client.new(:host => "fms.example.com",
+                        :apswd => "secret")
     c.get_apps
-
     assert_requested(:get, "http://fms.example.com:1111/admin/getApps?apswd=secret&auser=fms")
+  end
+
+  def test_should_use_params_to_build_url
+    c = FMS::Client.new(:host => 'fms.example.com')
+    c.get_apps(:force => true, :verbose => false)
+    assert_requested(:get, "http://fms.example.com:1111/admin/getApps?apswd=fms&auser=fms&force=true&verbose=false")
+  end
+
+  def test_should_camelize_parameters_to_build_url
+    c = FMS::Client.new(:host => 'fms.example.com')
+    c.reload_app(:app_inst => 'live/cam1')
+    assert_requested(:get, "http://fms.example.com:1111/admin/reloadApp?appInst=live/cam1&apswd=fms&auser=fms")
   end
 
 end
